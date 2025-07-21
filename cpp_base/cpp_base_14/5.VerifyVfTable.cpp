84d570ee8159d813dbccc558b2553385ec20e605
DoublePenetrate <884046262@qq.com>
2025-07-22 00:11:33 +0800

    C++_Base_Day14作业

---
+++ cpp_base/cpp_base_14/5.VerifyVfTable.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+
+using std::endl;
+using std::cin;
+using std::cout;
+
+class Base {
+public:
+    virtual void print() {
+        cout << "Base::print()" << endl;
+    }
+
+    virtual void display() {
+        cout << "Base::display()" << endl;
+    }
+
+    virtual void show() {
+        cout << "Base::show()" << endl;
+    }
+private:
+    long _base = 10;
+};
+
+class Derived : public Base {
+public:
+    virtual void print() {
+        cout << "Derived::print()" << endl;
+    }
+
+    virtual void display() {
+        cout << "Derived::display()" << endl;
+    }
+
+    virtual void show() {
+        cout << "Derived::show()" << endl;
+    }
+
+private:
+    long _derived = 100;
+};
+
+void test() {
+    Derived d;
+    long * pDerived = reinterpret_cast<long *>(&d);
+    cout << pDerived[0] << endl;
+    cout << pDerived[1] << endl;
+    cout << pDerived[2] << endl;
+
+    cout << endl;
+    long * pVtable = reinterpret_cast<long *>(pDerived[0]);
+    cout << pVtable[0] << endl;
+    cout << pVtable[1] << endl;
+    cout << pVtable[2] << endl;
+
+    cout << endl;
+    typedef void (* Function)();
+    Function f = (Function)(pVtable[0]);
+    f();
+    f = (Function)(pVtable[1]);
+    f();
+    f = (Function)(pVtable[2]);
+    f();
+}
+
+int main()
+{
+    test();
+    return 0;
+}
+
